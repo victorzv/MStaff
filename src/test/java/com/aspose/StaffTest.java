33be5455df346ce4fb2d7a2db4d4e3fdf9d045db
victorzv <kaplantigra@gmail.com>
2018-12-11 12:52:48 +0200

    add few tests

--- src/test/java/com/aspose/StaffTest.java
+++ src/test/java/com/aspose/StaffTest.java
@@ -18,7 +18,8 @@ import java.time.LocalDate;
 import java.util.Properties;
 
 public class StaffTest {
-    FactoryPerson factoryPerson;
+    FactoryPerson factoryPerson = new FactoryPerson();
+    CalculateSalary calculateSalary = new CalculateSalary();
 
     @Before
     public void createDataBeforeTest(){
@@ -39,9 +40,6 @@ public class StaffTest {
         Sales.ADD_PERSENT =  BigDecimal.valueOf(Double.valueOf(props.getProperty("SALES_ADD_PERSENT", "0.01")));
         Sales.ADD_PERSENT_MAX =  BigDecimal.valueOf(Double.valueOf(props.getProperty("SALES_ADD_PERSENT_MAX", "0.35")));
         Sales.SUB_PERSENT =  BigDecimal.valueOf(Double.valueOf(props.getProperty("SALES_SUB_PERSENT", "0.005")));
-
-
-        factoryPerson = new FactoryPerson();
     }
 
     @Test
@@ -57,24 +55,54 @@ public class StaffTest {
     }
 
     @Test
-    public void salaryShouldBeEquals(){
+    public void salaryEmployeeEquals(){
         IPerson E = factoryPerson.createPerson(TypePerson.EMPLOYEE, LocalDate.now().minusYears(1), "E1");
         Staff staff = new Staff();
         staff.addPerson(E);
         BigDecimal salary = E.getBaseRate().add(E.getBaseRate().multiply(E.getPersentAdd(), Algoritm.mc), Algoritm.mc);
         staff.prepareCalculate(E);
-        CalculateSalary calculateSalary = new CalculateSalary();
         calculateSalary.calculate(E, staff);
         Assert.assertEquals(salary, staff.getSalary(E));
     }
 
     @Test
     public void employeeCanNotBeChief(){
-        IPerson E1 = factoryPerson.createPerson(TypePerson.EMPLOYEE, LocalDate.of(2002, 11, 21), "E1");
-        IPerson M1 = factoryPerson.createPerson(TypePerson.MANAGER, LocalDate.of(2002, 11, 21), "M1");
+        IPerson E1 = factoryPerson.createPerson(TypePerson.EMPLOYEE, LocalDate.now().minusYears(1), "E1");
+        IPerson M1 = factoryPerson.createPerson(TypePerson.MANAGER, LocalDate.now().minusYears(1), "M1");
         Staff staff = new Staff();
         Assert.assertEquals(false, staff.addPesonChief(M1, E1));
     }
 
+    @Test
+    public void salaryManagerEquals(){
+        IPerson E1 = factoryPerson.createPerson(TypePerson.EMPLOYEE, LocalDate.now().minusYears(1), "E1");
+        IPerson M1 = factoryPerson.createPerson(TypePerson.MANAGER, LocalDate.now().minusYears(1), "M1");
+        Staff staff = new Staff();
+        staff.addPesonChief(E1, M1);
+
+        BigDecimal salaryE1 = E1.getBaseRate().add(E1.getBaseRate().multiply(E1.getPersentAdd(), Algoritm.mc), Algoritm.mc);
+        BigDecimal salaryM1 = M1.getBaseRate().add(M1.getBaseRate().multiply(M1.getPersentAdd(), Algoritm.mc), Algoritm.mc).add(salaryE1.multiply(M1.getSubPersentAdd(), Algoritm.mc), Algoritm.mc);
+        staff.prepareCalculate(M1);
+        calculateSalary.calculate(M1, staff);
+        Assert.assertEquals(salaryM1, staff.getSalary(M1));
+    }
+
+    @Test
+    public void salarySalesEquals(){
+        IPerson E1 = factoryPerson.createPerson(TypePerson.EMPLOYEE, LocalDate.now().minusYears(1), "E1");
+        IPerson M1 = factoryPerson.createPerson(TypePerson.MANAGER, LocalDate.now().minusYears(1), "M1");
+        IPerson S1 = factoryPerson.createPerson(TypePerson.SALES, LocalDate.now().minusYears(1), "S1");
+
+        Staff staff = new Staff();
+        staff.addSubsToChief(S1, E1, M1);
+
+        BigDecimal salaryE1 = E1.getBaseRate().add(E1.getBaseRate().multiply(E1.getPersentAdd(), Algoritm.mc), Algoritm.mc);
+        BigDecimal salaryM1 = M1.getBaseRate().add(M1.getBaseRate().multiply(M1.getPersentAdd(), Algoritm.mc), Algoritm.mc).add(salaryE1.multiply(M1.getSubPersentAdd(), Algoritm.mc), Algoritm.mc);
+        BigDecimal salaryM1plusE1 = salaryE1.add(salaryM1, Algoritm.mc);
+        BigDecimal salaryS1 = S1.getBaseRate().add(S1.getBaseRate().multiply(S1.getPersentAdd(), Algoritm.mc), Algoritm.mc).add(salaryM1plusE1.multiply(S1.getSubPersentAdd(), Algoritm.mc), Algoritm.mc);
+        staff.prepareCalculate(S1);
+        calculateSalary.calculate(S1, staff);
+        Assert.assertEquals(salaryS1, staff.getSalary(S1));
+    }
 
 }
